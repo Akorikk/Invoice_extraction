00b0f1138dabc669222ead070149d8ea6852ae56
Abhishek Kori <abhishekkori601@gmail.com>
2025-12-07 19:41:24 +0530

    utils

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "oil_retail",
+    name = "invoice_extraction",
     version="0.0.1",
     author="Abhishek",
     author_email="abhishekkori601@gmail.com",
